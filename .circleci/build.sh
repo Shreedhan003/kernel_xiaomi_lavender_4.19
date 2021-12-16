b3ec1a7caa212d4a8753db5872a7f1c3f7c184ab
Shridhan Varadkar <shridhan98@gmail.com>
2021-12-16 19:47:13 +0530

    Update build.sh

--- .circleci/build.sh
+++ .circleci/build.sh
@@ -1,21 +1,23 @@
 #!/usr/bin/env bash
 echo "Cloning dependencies"
 git clone --depth=1 https://github.com/Shreedhan003/proton-clang.git clang
+git clone https://github.com/sohamxda7/llvm-stable -b gcc64 --depth=1 gcc
+git clone https://github.com/sohamxda7/llvm-stable -b gcc32  --depth=1 gcc32
 git clone --depth=1 https://github.com/Shreedhan003/AnyKernel3.git AnyKernel
 echo "Done"
 IMAGE=$(pwd)/out/arch/arm64/boot/Image.gz-dtb
 TANGGAL=$(date +"%F-%S")
 START=$(date +"%s")
 KERNEL_DIR=$(pwd)
-PATH="${KERNEL_DIR}/clang/bin:${KERNEL_DIR}/clang/aarch64-linux-gnu/bin:${KERNEL_DIR}/clang/arm-linux-gnueabi/bin:${PATH}"
+PATH="${KERNEL_DIR}/clang/bin:${KERNEL_DIR}/gcc/bin:${KERNEL_DIR}/gcc32/bin:${PATH}"
 export KBUILD_COMPILER_STRING="$(${KERNEL_DIR}/clang/bin/clang --version | head -n 1 | perl -pe 's/\(http.*?\)//gs' | sed -e 's/  */ /g')"
+export ARCH=arm64
 export KBUILD_BUILD_HOST=circleci
 export KBUILD_BUILD_USER="shridhan"
-export ARCH=arm64
 # sticker plox
 function sticker() {
     curl -s -X POST "https://api.telegram.org/bot$token/sendSticker" \
-        -d sticker="CAACAgEAAxkBAAEnKnJfZOFzBnwC3cPwiirjZdgTMBMLRAACugEAAkVfBy-aN927wS5blhsE" \
+        -d sticker="CAACAgEAAxkBAAEDdzthtZ4zC7lmavk5slORixgAAWJ6TUIAAqgBAAI3kMBHHc2t-Bef-s0jBA" \
         -d chat_id=$chat_id
 }
 # Send info plox channel
@@ -47,12 +49,13 @@ function finerr() {
 }
 # Compile plox
 function compile() {
-    make O=out ARCH=arm64 lavender_defconfig
+    make O=out ARCH=arm64 lavender-perf_defconfig
     make -j$(nproc --all) O=out \
                     ARCH=arm64 \
                     CC=clang \
+                    CLANG_TRIPLE=aarch64-linux-gnu- \
                     CROSS_COMPILE=aarch64-linux-android- \
-                    CROSS_COMPILE_ARM32=arm-linux-androideabi- \
+                    CROSS_COMPILE_ARM32=arm-linux-androideabi-
 
     if ! [ -a "$IMAGE" ]; then
         finerr
@@ -63,7 +66,7 @@ function compile() {
 # Zipping
 function zipping() {
     cd AnyKernel || exit 1
-    zip -r9 RK-4.19-${TANGGAL}.zip *
+    zip -r9 Raven-Kernel-lavender-${TANGGAL}.zip *
     cd ..
 }
 sticker
