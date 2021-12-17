572a496ffc098c2d1a7ba071fe982b9e7f0b3c8c
Shridhan Varadkar <shridhan98@gmail.com>
2021-12-17 22:43:43 +0530

    Update build.sh

--- .circleci/build.sh
+++ .circleci/build.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 echo "Cloning dependencies"
 git clone --depth=1 https://github.com/Shreedhan003/proton-clang.git clang
-git clone https://android.googlesource.com/platform/prebuilts/gcc/linux-x86/aarch64/aarch64-linux-android-4.9 --depth=1 gcc
-git clone https://android.googlesource.com/platform/prebuilts/gcc/linux-x86/arm/arm-linux-androideabi-4.9 --depth=1 gcc32
+git clone https://github.com/sohamxda7/llvm-stable -b gcc64 --depth=1 gcc
+git clone https://github.com/sohamxda7/llvm-stable -b gcc32  --depth=1 gcc32
 git clone --depth=1 https://github.com/Shreedhan003/AnyKernel3.git AnyKernel
 echo "Done"
 IMAGE=$(pwd)/out/arch/arm64/boot/Image.gz-dtb
